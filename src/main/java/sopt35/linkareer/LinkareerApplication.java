eca9af1911db06cb9842ae5b8dfe326bc5b14256
Khyojun <khyojun1104@gmail.com>
2024-11-19 00:50:17 +0900

    [init] init project

---
+++ src/main/java/sopt35/linkareer/LinkareerApplication.java
@@ -0,0 +1,13 @@
+package sopt35.linkareer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkareerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkareerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sopt35/linkareer/LinkareerApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt35.linkareer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkareerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
