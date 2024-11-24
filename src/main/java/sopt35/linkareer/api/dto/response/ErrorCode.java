041af022f167ca4ff42a96078b41ac3af66b794f
Khyojun <khyojun1104@gmail.com>
2024-11-24 17:17:16 +0900

    [feat] #16 잘못된 헤더 요청 관련 에러코드 추가

--- src/main/java/sopt35/linkareer/api/dto/response/ErrorCode.java
+++ src/main/java/sopt35/linkareer/api/dto/response/ErrorCode.java
@@ -5,7 +5,8 @@ public enum ErrorCode {
     INVALID_INPUT_CATEGORY_VALUE("카테고리는 필수 항목입니다."),
     INVALID_INPUT_OFFICIAL_VALUE("잘못된 공고 리스트 요청입니다."),
     CATEGORY_NOT_FOUND("존재하지 않는 카테고리입니다."),
-    INTERNAL_SERVER_ERROR("서버 내부 오류입니다.");
+    INTERNAL_SERVER_ERROR("서버 내부 오류입니다."),
+    INVALID_HEADER_EXCEPTION("잘못된 헤더 요청입니다.");
 
     private final String message;
 
